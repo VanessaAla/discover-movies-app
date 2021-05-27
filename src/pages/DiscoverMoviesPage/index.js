a14089804e9aacd71492fe3086159acbbf3bc5ae
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-05-27 16:51:47 +0200

    created searchstate for discover a movie

--- src/pages/DiscoverMoviesPage/index.js
+++ src/pages/DiscoverMoviesPage/index.js
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function DiscoverMoviesPage() {
   const [searchText, set_searchText] = useState("");
+  const [searchState, set_searchState] = useState("");
 
-  const search = () => {
-    console.log("TODO search movies for:", searchText);
+  const search = async (event) => {
+    event.preventDefault();
+    if (searchText === "") {
+      searchState({ status: "idle" });
+      return;
+    }
+    set_searchState({ status: "searching" });
+    const queryParam = encodeURIComponent(searchText);
+
+    const response = await axios.get(
+      `https://omdbapi.com/?apikey=8679fb8e&s=${queryParam}`
+    );
+
+    console.log("Response!", response);
+    set_searchState({ status: "Done!" });
   };
 
   return (
